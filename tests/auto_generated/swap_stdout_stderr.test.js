7f4c2c1acdf0339250472c8d6bb9a5a36954a9df
ts-klassen <git@su-shiki.com>
2025-06-15 02:46:33 +0900

    tests: swap stdout and stderr in swap_stdout_stderr test using FD redirections

--- tests/auto_generated/swap_stdout_stderr.test.js
+++ tests/auto_generated/swap_stdout_stderr.test.js
@@ -4,10 +4,10 @@ const klsh = require('../../dist/klsh.js');
 
 describe('auto-generated swap_stdout_stderr', function() {
   it("bash: klsh", async function() {
-    const stdin = "echo data > /tmp/swap_stdout_stderr.txt\ncat /tmp/swap_stdout_stderr.txt /tmp/swap_stdout_stderr_non_existing.txt\n";
+    const stdin = "echo data > /tmp/swap_stdout_stderr.txt\ncat /tmp/swap_stdout_stderr.txt /tmp/swap_stdout_stderr_non_existing.txt 3>&1 4>&2 5>&3 6>&4 1>&6 2>&5\n";
     const result = await klsh.klsh.main({ stdin: "klsh", input: stdin, env: {} });
-    expect(result.stdout).to.equal("data\n");
-    expect(result.stderr).to.equal("cat: /tmp/swap_stdout_stderr_non_existing.txt: No such file or directory\n");
+    expect(result.stdout).to.equal("cat: /tmp/swap_stdout_stderr_non_existing.txt: No such file or directory\n");
+    expect(result.stderr).to.equal("data\n");
     expect(result.env['?']).to.equal(1);
   });
 });
